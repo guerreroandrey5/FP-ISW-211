0b62d322228e134e5ca88ba3858404af4016b3ae
Guerrero <Guerrero@GuerreroPC>
2021-08-03 22:14:54 -0600

    Casi terminado, aún faltan muchas cosas pero ya hace lo principal xd

--- src/main/java/Gato/Gato.java
+++ src/main/java/Gato/Gato.java
@@ -5,17 +5,146 @@
  */
 package Gato;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author guerreroandrey5
  */
 public class Gato extends javax.swing.JFrame {
 
+    String equis = "X";
+    int Contador;
+    int ContadorB;
+
     /**
      * Creates new form Gato
      */
     public Gato() {
         initComponents();
+    }
+
+    public void ganar() {
+        String B1 = Btn1.getText();
+        String B2 = Btn2.getText();
+        String B3 = Btn3.getText();
+        String B4 = Btn4.getText();
+        String B5 = Btn5.getText();
+        String B6 = Btn6.getText();
+        String B7 = Btn7.getText();
+        String B8 = Btn8.getText();
+        String B9 = Btn9.getText();
+        //Condicioneas para el primer jugador
+        if (B1.equals("X") && B2.equals("X") && B3.equals("X")){
+            ganaJ1();
+        }
+        if (B4.equals("X") && B5.equals("X") && B6.equals("X")){
+            ganaJ1();
+        }
+        if (B7.equals("X") && B8.equals("X") && B9.equals("X")){
+            ganaJ1();
+        }
+        if (B1.equals("X") && B4.equals("X") && B7.equals("X")){
+            ganaJ1();
+        }
+        if (B2.equals("X") && B5.equals("X") && B8.equals("X")){
+            ganaJ1();
+        }
+        if (B3.equals("X") && B6.equals("X") && B9.equals("X")){
+            ganaJ1();
+        }
+        if (B3.equals("X") && B5.equals("X") && B7.equals("X")){
+            ganaJ1();
+        }
+        if (B1.equals("X") && B5.equals("X") && B9.equals("X")){
+            ganaJ1();
+        }
+        //Condicioneas para el segundo jugador
+        
+        if (B1.equals("O") && B2.equals("O") && B3.equals("O")){
+            ganaJ2();
+        }
+        if (B4.equals("O") && B5.equals("O") && B6.equals("O")){
+            ganaJ2();
+        }
+        if (B7.equals("O") && B8.equals("O") && B9.equals("O")){
+            ganaJ2();
+        }
+        if (B1.equals("O") && B4.equals("O") && B7.equals("O")){
+            ganaJ2();
+        }
+        if (B2.equals("O") && B5.equals("O") && B8.equals("O")){
+            ganaJ2();
+        }
+        if (B3.equals("O") && B6.equals("O") && B9.equals("O")){
+            ganaJ2();
+        }
+        if (B3.equals("O") && B5.equals("O") && B7.equals("O")){
+            ganaJ2();
+        }
+        if (B1.equals("O") && B5.equals("O") && B9.equals("O")){
+            ganaJ2();
+        }
+    }
+
+    public void empatar() {
+        String B1 = Btn1.getText();
+        String B2 = Btn2.getText();
+        String B3 = Btn3.getText();
+        String B4 = Btn4.getText();
+        String B5 = Btn5.getText();
+        String B6 = Btn6.getText();
+        String B7 = Btn7.getText();
+        String B8 = Btn8.getText();
+        String B9 = Btn9.getText();
+            
+        if(B1 != "" && B2 != "" && B3 != "" &&
+           B4 != "" && B5 != "" && B6 != "" && 
+           B7 != "" && B8 != "" && B9 != ""){
+            JOptionPane.showMessageDialog(null, "El juego está empatado");
+            nGame();
+        }
+    }
+
+    public void ganaJ1() {
+        JOptionPane.showMessageDialog(null, "Ha ganado el Jugador 1");
+        nGame();
+        Contador +=1;
+        String ContStr = String.valueOf(Contador);
+        LblPts1.setText(ContStr);
+    }
+
+    public void ganaJ2() {
+        JOptionPane.showMessageDialog(null, "Ha ganado el Jugador 2");
+        nGame();
+        ContadorB +=1;
+        String ContStr = String.valueOf(ContadorB);
+        LblPts2.setText(ContStr);
+    }
+
+    public void nGame() {       
+        //Resetea todos los botones y borra su texto
+        Btn1.setEnabled(true);
+        Btn1.setText("");
+        Btn2.setEnabled(true);
+        Btn2.setText("");
+        Btn3.setEnabled(true);
+        Btn3.setText("");
+        Btn4.setEnabled(true);
+        Btn4.setText("");
+        Btn5.setEnabled(true);
+        Btn5.setText("");
+        Btn6.setEnabled(true);
+        Btn6.setText("");
+        Btn7.setEnabled(true);
+        Btn7.setText("");
+        Btn8.setEnabled(true);
+        Btn8.setText("");
+        Btn9.setEnabled(true);
+        Btn9.setText("");
+        equis = "X";
     }
 
     /**
@@ -27,21 +156,314 @@ public class Gato extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        Btn1 = new javax.swing.JButton();
+        Btn2 = new javax.swing.JButton();
+        Btn3 = new javax.swing.JButton();
+        Btn4 = new javax.swing.JButton();
+        Btn5 = new javax.swing.JButton();
+        Btn6 = new javax.swing.JButton();
+        Btn7 = new javax.swing.JButton();
+        Btn8 = new javax.swing.JButton();
+        Btn9 = new javax.swing.JButton();
+        BtnNew = new javax.swing.JButton();
+        LblHora = new javax.swing.JLabel();
+        Lblh = new javax.swing.JLabel();
+        LblFHoy = new javax.swing.JLabel();
+        Lblf = new javax.swing.JLabel();
+        LblP1 = new javax.swing.JLabel();
+        LblP2 = new javax.swing.JLabel();
+        LblPts1 = new javax.swing.JLabel();
+        LblPts2 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setLayout(new java.awt.GridLayout(3, 3));
+
+        Btn1.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn1);
+
+        Btn2.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn2);
+
+        Btn3.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn3);
+
+        Btn4.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn4);
+
+        Btn5.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn5ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn5);
+
+        Btn6.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn6ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn6);
+
+        Btn7.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn7ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn7);
+
+        Btn8.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn8ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn8);
+
+        Btn9.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
+        Btn9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn9ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Btn9);
+
+        BtnNew.setText("Nuevo Juego");
+        BtnNew.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnNewActionPerformed(evt);
+            }
+        });
+
+        LblHora.setText("Hora:");
+
+        Lblh.setText("h");
+
+        LblFHoy.setText("Fecha de Juego:");
+
+        Lblf.setText("fhoy");
+
+        LblP1.setText("Jugador 1:");
+
+        LblP2.setText("Jugador 2:");
+
+        LblPts1.setText("0");
+
+        LblPts2.setText("0");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 302, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(59, 59, 59))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(LblFHoy)
+                                .addGap(18, 18, 18)
+                                .addComponent(Lblf)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(LblHora)
+                                .addGap(18, 18, 18)
+                                .addComponent(Lblh)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(BtnNew)
+                                .addGap(42, 42, 42)
+                                .addComponent(LblP2))
+                            .addComponent(LblP1))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(LblPts1)
+                            .addComponent(LblPts2))
+                        .addGap(53, 53, 53))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LblP1)
+                            .addComponent(LblPts1))
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LblP2)
+                            .addComponent(LblPts2)
+                            .addComponent(BtnNew)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LblFHoy)
+                            .addComponent(Lblf))
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LblHora)
+                            .addComponent(Lblh))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(45, 45, 45))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void Btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn2ActionPerformed
+        Btn2.setText(equis);
+        if (equis.equals("X")) {
+            Btn2.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn2.setEnabled(false);
+        }         
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn2ActionPerformed
+
+    private void Btn5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn5ActionPerformed
+        Btn5.setText(equis);
+        if (equis.equals("X")) {
+            Btn5.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn5.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn5ActionPerformed
+
+    private void Btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn1ActionPerformed
+        Btn1.setText(equis);
+        if (equis.equals("X")) {
+            Btn1.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn1.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn1ActionPerformed
+
+    private void Btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn3ActionPerformed
+        Btn3.setText(equis);
+        if (equis.equals("X")) {
+            Btn3.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn3.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn3ActionPerformed
+
+    private void Btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn4ActionPerformed
+        Btn4.setText(equis);
+        if (equis.equals("X")) {
+            Btn4.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn4.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn4ActionPerformed
+
+    private void Btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn6ActionPerformed
+        Btn6.setText(equis);
+        if (equis.equals("X")) {
+            Btn6.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn6.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn6ActionPerformed
+
+    private void Btn7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn7ActionPerformed
+        Btn7.setText(equis);
+        if (equis.equals("X")) {
+            Btn7.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn7.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn7ActionPerformed
+
+    private void Btn8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn8ActionPerformed
+        Btn8.setText(equis);
+        if (equis.equals("X")) {
+            Btn8.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn8.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn8ActionPerformed
+
+    private void Btn9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn9ActionPerformed
+        Btn9.setText(equis);
+        if (equis.equals("X")) {
+            Btn9.setEnabled(false);
+            equis = "O";
+        } else {
+            equis = "X";
+            Btn9.setEnabled(false);
+        }
+            ganar();
+            empatar();
+    }//GEN-LAST:event_Btn9ActionPerformed
+
+    private void BtnNewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnNewActionPerformed
+        LblPts1.setText("0");
+        LblPts2.setText("0");
+        Contador = 0;
+        ContadorB = 0;
+    }//GEN-LAST:event_BtnNewActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +501,24 @@ public class Gato extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Btn1;
+    private javax.swing.JButton Btn2;
+    private javax.swing.JButton Btn3;
+    private javax.swing.JButton Btn4;
+    private javax.swing.JButton Btn5;
+    private javax.swing.JButton Btn6;
+    private javax.swing.JButton Btn7;
+    private javax.swing.JButton Btn8;
+    private javax.swing.JButton Btn9;
+    private javax.swing.JButton BtnNew;
+    private javax.swing.JLabel LblFHoy;
+    private javax.swing.JLabel LblHora;
+    private javax.swing.JLabel LblP1;
+    private javax.swing.JLabel LblP2;
+    private javax.swing.JLabel LblPts1;
+    private javax.swing.JLabel LblPts2;
+    private javax.swing.JLabel Lblf;
+    private javax.swing.JLabel Lblh;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
