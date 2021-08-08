c04bad3b2a24599cc76f146cca5140d012bc7e7a
Cris <Cris@DESKTOP-QEEV68G>
2021-08-08 15:09:10 -0600

    General que olvide agregar xd

---
+++ src/main/java/General/Resize.java
@@ -0,0 +1,32 @@
+package General;
+
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cris
+ */
+public class Resize {
+
+    public Resize() {
+       
+    }
+    
+    /*Changes the size of the images and return a new one*/
+    public ImageIcon Resize(ImageIcon img, JLabel dimensions) {
+        
+        Image Oimg = img.getImage().getScaledInstance(dimensions.getWidth(), dimensions.getHeight(), Image.SCALE_REPLICATE);
+        ImageIcon newIMG = new ImageIcon(Oimg);
+        
+        return newIMG;
+    }
+}
