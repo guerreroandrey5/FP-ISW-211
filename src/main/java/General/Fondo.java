a02dae713861d8824b46aaf73532730087bbe52f
Cris <Cris@DESKTOP-QEEV68G>
2021-08-15 16:12:46 -0600

    comentarios agregados

--- src/main/java/General/Fondo.java
+++ src/main/java/General/Fondo.java
@@ -16,13 +16,15 @@ import javax.swing.JLabel;
  * @author Cris
  */
 public class Fondo {
-    private Resize Ajustar = new Resize();
-    
-    //Set a image as brackground into a Frame
+
+    private Resize Ajustar = new Resize(); //Inicialize a Resize class to ajust the image to the JFram
+   
+    //A Funtion that recive the name of the image, and the designated JFrame to set the background image
     public Fondo(String imagen, JFrame frame) {
-        ImageIcon icon = Ajustar.ResizeJF(new ImageIcon(".\\src\\main\\java\\Recursos/" + imagen), frame);
-        frame.setLayout(new BorderLayout());
-        frame.setContentPane(new JLabel(icon));
-        frame.setLayout(new FlowLayout());
+        ImageIcon icon = Ajustar.ResizeJF(new ImageIcon(".\\src\\main\\java\\Recursos/" + imagen), frame); //Call the function ResizeJF to ajuste the image width and height
+        frame.setLayout(new BorderLayout()); //Set the Frame layaout to border layout
+        frame.setContentPane(new JLabel(icon)); //Set creates a content panel with the size of the Frame to asing the background
+        frame.setLayout(new FlowLayout()); //Revert the layout to flowLayout
+
     }
 }
